6ee3c88580c718fe29423df34dc5891dc484c1a2
angelgt <angel.gamonal@agenciaservitec.com>
2023-10-23 19:14:35 -0500

    add handleUpdateBoard

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,46 @@ const TURNS = {
 	X: 'x',
 	O: 'O',
 };
-const Square = ({ children, updateBoard, index }) => (
-	<div
-		className="square"
-		onClick={() => {
-			console.log('hola');
-		}}
-	>
-		{children}
-	</div>
-);
+const Square = ({ children, updateBoard, index, isSelect = '' }) => {
+	return (
+		<div className={`square ${isSelect}`} onClick={updateBoard}>
+			{children}
+		</div>
+	);
+};
 
 function App() {
 	const [turn, setTurn] = useState(TURNS.X);
 	const [board, setBoard] = useState(Array(9).fill(null));
 
+	const handleUpdateBoard = () => {
+		setTurn(turn === TURNS.X ? TURNS.O : TURNS.X);
+	};
+	console.log(turn);
+
 	return (
 		<>
 			<main className="board">
-				<h1
-					onClick={() => {
-						console.log(turn);
-						setTurn(TURNS.O);
-					}}
-				>
-					Tic tac toe
-				</h1>
+				<h1>Tic tac toe</h1>
 				<section className="game">
 					{board.map((_, index) => (
-						<Square key={index} index={index}>
-							{_}
+						<Square key={index} index={index} updateBoard={handleUpdateBoard}>
+							{index}
 						</Square>
 					))}
+				</section>
+
+				<section
+					className="turn"
+					style={{
+						margin: '2em auto',
+						display: 'flex',
+						justifyContent: 'center',
+						gap: '10px',
+					}}
+				>
+					<Square isSelect="is-selected">X</Square>
+					<Square>Y</Square>
 				</section>
 			</main>
 		</>
